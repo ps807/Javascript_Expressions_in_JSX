416f726c236c053645547f0253e95f9ca5ab0ff6
ps807 <66031843+ps807@users.noreply.github.com>
2020-09-25 13:43:33 +0200

    Lucky number

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const luckyNumber = 8;
 ReactDOM.render(
   <div>
     <h1>Hello {name}!</h1>
-    <p>Your lucky number is {luckyNumber}</p>
+    <p>Your lucky number is {Math.floor(Math.random() * 9) + 1}</p>
   </div>,
   document.getElementById("root")
 );
